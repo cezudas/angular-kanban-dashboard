0ca9bf2799c5dba2f21efe83ded9e0ff9fc3a740
Cezar Dascal <cezar.dascal@she.net>
2022-02-24 08:57:43 +0200

    initial commit

---
+++ apps/kanban-dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vantage-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kanban-dashboard';
+}
